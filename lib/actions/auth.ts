b5478eb4934f93a2e36cc2f12b71bba9023d4eaf
muhammadrayhanr <mrayhanr4869@gmail.com>
2025-01-23 01:42:32 +0700

    refactor: disable workflowClient on lib/actions/lib/auth.ts

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,8 +8,8 @@ import { eq } from 'drizzle-orm';
 import { headers } from 'next/headers';
 import ratelimit from '@/lib/ratelimit';
 import { redirect } from 'next/navigation';
-import { workflowClient } from '@/lib/workflow';
-import config from '@/lib/config';
+// import { workflowClient } from '@/lib/workflow';
+// import config from '@/lib/config';
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, 'email' | 'password'>
@@ -68,13 +68,13 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
     });
 
-    await workflowClient.trigger({
-      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
-      body: {
-        email,
-        fullName,
-      },
-    });
+    // await workflowClient.trigger({
+    //   url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+    //   body: {
+    //     email,
+    //     fullName,
+    //   },
+    // });
 
     await signInWithCredentials({ email, password });
     return { success: true };
